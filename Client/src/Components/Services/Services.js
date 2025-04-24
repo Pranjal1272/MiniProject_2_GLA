78af7c1cff604cfedf1431b6f1c38783126d863c
Racheet Saraswat <racheet.saraswat_cs22@gla.ac.in>
2025-04-25 01:21:25 +0530

    Restructured project: removed old web/ directory, added Client and server directories

---
+++ Client/src/Components/Services/Services.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PostPetSection from './PostPetSection'
+
+const Services = () => {
+  return (
+    <div className='main-container'>
+        
+        <div className='post-pet'>
+            <PostPetSection/>
+        </div>
+    </div>
+  )
+}
+
+export default Services
